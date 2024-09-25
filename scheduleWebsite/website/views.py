e2affde7be44326dcf355a2e24415c8d080f32d3
MA0610 <47308923+MA0610@users.noreply.github.com>
2024-09-25 11:27:43 -0700

    Delete scheduleWebsite/website directory

--- scheduleWebsite/website/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-views = Blueprint('views',__name__)
-
-#defining root/home page
-@views.route('/')
-def home():
-    return render_template("home.html")
