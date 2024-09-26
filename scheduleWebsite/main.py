f672e960ce87a1e1d6b1f5f82140733516f3ff5a
MA0610 <47308923+MA0610@users.noreply.github.com>
2024-09-25 21:44:11 -0700

    Add files via upload
    user auth is setup

--- scheduleWebsite/main.py
+++ scheduleWebsite/main.py
@@ -84,33 +84,33 @@ if __name__ == '__main__':
 #x is num of rows (time slots on given day: ?)
 #y is num of columns (number of days in week: 5)
 #z is num of entries in array (lists: 1)
-def create_3d_list(x,y,z):
-    lst = []
-    for i in range(x):
-        lst_2d = []
-        for j in range(y):
-            lst_1d = []
-            for k in range(z):
-                lst_1d.append([])
-            lst_2d.append(lst_1d)
-        lst.append(lst_2d)
-    return lst
+# def create_3d_list(x,y,z):
+#     lst = []
+#     for i in range(x):
+#         lst_2d = []
+#         for j in range(y):
+#             lst_1d = []
+#             for k in range(z):
+#                 lst_1d.append([])
+#             lst_2d.append(lst_1d)
+#         lst.append(lst_2d)
+#     return lst
 
 
-test = create_3d_list(3, 5, 1)
+# test = create_3d_list(3, 5, 1)
 
-test[0][0][0].append("3")
-test[0][0][0].append("4")
-
-
-##
+# test[0][0][0].append("3")
+# test[0][0][0].append("4")
 
 
+# ##
 
 
 
-# used the pretty printed function
-pprint.pprint(test)
+
+
+# # used the pretty printed function
+# pprint.pprint(test)
 
 
 
