1d3005cf3fbf728d9f6325a6c66850074c90c9de
MA0610 <47308923+MA0610@users.noreply.github.com>
2024-10-01 13:42:18 -0700

    Add files via upload

--- scheduleWebsite/website/__init__.py
+++ scheduleWebsite/website/__init__.py
@@ -1,14 +1,59 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+       
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'sldadks lkjlkas'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
-    from .app import app
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    # from .models import User, Note
+    from .models import User, Schedule
+
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+
+#x is num of rows (time slots on given day: 20)
+#y is num of columns (number of days in week: 5)
+#z is num of entries in array (lists: 1)
+def create_3d_list(x,y,z):
+    lst = []
+    for i in range(x):
+        lst_2d = []
+        for j in range(y):
+            lst_1d = []
+            for k in range(z):
+                lst_1d.append([])
+            lst_2d.append(lst_1d)
+        lst.append(lst_2d)
+    return lst    
--- scheduleWebsite/website/views.py
+++ scheduleWebsite/website/views.py
@@ -1,8 +1,54 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Schedule
+from . import db
+import json
+import numpy as np
 
-views = Blueprint('views',__name__)
+views = Blueprint('views', __name__)
 
-#defining root/home page
-@views.route('/')
+def test():
+    print("TEST")
+
+#x is num of rows (time slots on given day: ?)
+#y is num of columns (number of days in week: 5)
+#z is num of entries in array (lists: 1)
+def create_3d_list(x,y,z):
+    lst = []
+    for i in range(x):
+        lst_2d = []
+        for j in range(y):
+            lst_1d = []
+            for k in range(z):
+                lst_1d.append([])
+            lst_2d.append(lst_1d)
+        lst.append(lst_2d)
+    return lst
+
+
+schedule = create_3d_list(20,5,1)
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        className = request.form.get('class')
+        if(className=='Math 101'):
+            schedule[0,0,0] = className
+            flash('Logged in successfully!', category='success')
+
+#HERE
+    return render_template("home.html", user=current_user, test=test)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_class():  
+    class1 = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    classId = class1['classId']
+    class1 = Schedule.query.get(classId)
+    if class1:
+        if class1.user_id == current_user.id:
+            db.session.delete(class1)
+            db.session.commit()
+
+    return jsonify({})
