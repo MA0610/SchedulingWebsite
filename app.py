c897c7c211b6622f81374b161606a98b36464acc
MA0610 <47308923+MA0610@users.noreply.github.com>
2024-10-01 15:57:58 -0700

    site data is sent to dictionary in python

---
+++ app.py
@@ -0,0 +1,49 @@
+# app.py
+from flask import Flask, request, jsonify, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+
+views = Blueprint('views',__name__)
+
+app = Flask(__name__)
+
+# Store schedules
+schedules = {
+    "Monday": [],
+    "Tuesday": [],
+    "Wednesday": [],
+    "Thursday": [],
+    "Friday": []
+}
+
+
+
+
+
+#go here to check if values are being uploaded
+@app.route('/view_schedules', methods=['GET'])
+def view_schedules():
+    return jsonify(schedules)
+
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/schedule', methods=['POST'])
+def schedule():
+    data = request.json
+    day = data.get('day')
+    class_name = data.get('class')
+    
+    if day in schedules:
+        schedules[day].append(class_name)
+        return jsonify(success=True, schedule=schedules[day])
+    return jsonify(success=False)
+
+@app.route('/schedules', methods=['GET'])
+def get_schedules():
+    return jsonify(schedules)
+
+if __name__ == '__main__':
+    app.run(debug=True)
